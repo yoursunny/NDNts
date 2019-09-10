525581f750781ad290dd1412664aa659fbe51d10
Junxiao Shi <git@mail1.yoursunny.com>
2019-09-09 21:35:07 -0400

    ndn: placeholder

---
+++ packages/ndn/src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Distill the essence of NDN into one emoji.
+ * @returns URI of the emoji
+ * @todo Use an NDN Name instead of HTTPS.
+ */
+export function distill(): string {
+  return "https://emoji.slack-edge.com/T8FE5BT1Q/le-hand/1401b80be324ad50.png";
+}
