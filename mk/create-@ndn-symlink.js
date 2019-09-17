66c90d3ed65b8d588fc55d25e783e46b2e2e5cdc
Junxiao Shi <git@mail1.yoursunny.com>
2019-09-17 10:38:26 -0400

    mk: bootstrap with 'pnpm install -r'

--- mk/create-@ndn-symlink.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-
-if (!fs.existsSync("packages/node_modules")) {
-  fs.mkdirSync("packages/node_modules");
-}
-if (!fs.existsSync("packages/node_modules/@ndn")) {
-  fs.symlinkSync("..", "packages/node_modules/@ndn", "dir");
-}
