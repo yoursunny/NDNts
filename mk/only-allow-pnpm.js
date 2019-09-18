379b4a6e41a0736eb747104abcbcee8206162004
Junxiao Shi <git@mail1.yoursunny.com>
2019-09-18 18:53:22 -0400

    mk: run tsc via pnpm instead of lerna

--- mk/only-allow-pnpm.js
+++ mk/only-allow-pnpm.js
@@ -1,4 +1,4 @@
 if (!/^pnpm/.test(process.env.npm_config_user_agent)) {
-  console.log("Use `npx pnpm install` to install dependencies in this repository\n");
+  console.log("Use `npm run bootstrap` to install dependencies in this repository\n");
   process.exit(1);
 }
