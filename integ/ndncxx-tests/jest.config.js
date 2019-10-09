79b50667935c052053cae707c999a41f5fbe1ade
Junxiao Shi <git@mail1.yoursunny.com>
2019-10-09 00:43:32 -0400

    mk: upgrade to pnpm 4.0.0

--- integ/ndncxx-tests/jest.config.js
+++ integ/ndncxx-tests/jest.config.js
@@ -2,7 +2,7 @@ const parent = require("../../jest.config.js");
 
 const config = {
   ...parent,
-  testPathIgnorePatterns = parent.testPathIgnorePatterns.filter(x => !/integ/.test(x)),
+  testPathIgnorePatterns: parent.testPathIgnorePatterns.filter(x => !/integ/.test(x)),
 };
 
 module.exports = config;
