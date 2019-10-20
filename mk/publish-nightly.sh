4d181258c62ee291486780d9633b6fe4e459b871
Junxiao Shi <git@mail1.yoursunny.com>
2019-10-19 21:35:25 -0400

    mk: publish nightly builds

---
+++ mk/publish-nightly.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+verdaccio -c mk/verdaccio-nightly.yaml &
+VERDACCIO_PID=$!
+export npm_config_registry=http://127.0.0.1:64448
+VERSION=0.0.$(date +%Y%m%d)-nightly.$(git log --pretty=format:'%h' -n 1)
+pnpm recursive exec --filter ./packages -- bash -c 'npm version '$VERSION' && pnpm publish'
+kill $VERDACCIO_PID
+
+mkdir -p mk/nightly-output/
+for TARBALL in $(find mk/nightly-temp/ -name '*.tgz'); do
+  cp $TARBALL mk/nightly-output/$(basename $(dirname $TARBALL)).tgz
+done
+
+pushd mk/nightly-output/ >/dev/null
+(
+  echo '<!doctype html>'
+  echo '<title>NDNts nightly build</title>'
+  echo '<h1>NDNts nightly build '$VERSION'</h1>'
+  echo '<p><a href="https://yoursunny.com/p/NDNts/">NDNts homepage</a></p>'
+  echo '<pre>'
+  ls *.tgz
+  echo '</pre>'
+) > index.html
+popd >/dev/null
