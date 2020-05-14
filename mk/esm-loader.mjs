6e0f48ebdeb8cc9969ac17680b7d4a4edf0baf1f
Junxiao Shi <git@mail1.yoursunny.com>
2020-05-14 11:22:46 -0400

    mk: upgrade to @k-foss/ts-esnode 1.4.1

--- mk/esm-loader.mjs
+++ mk/esm-loader.mjs
@@ -1,7 +1,6 @@
-export { getFormat, transformSource } from "@k-foss/ts-esnode";
+export { dynamicInstantiate, getFormat, transformSource } from "@k-foss/ts-esnode";
 import { getTSConfig } from "@k-foss/ts-esnode/out/dist/Utils.js";
 import { promises as fs } from "fs";
-import { createRequire } from "module";
 import { dirname, resolve as pathResolve } from "path";
 import { fileURLToPath, pathToFileURL } from "url";
 
@@ -28,35 +27,4 @@ export async function resolve(specifier, context, defaultResolve) {
     specifier = pathToFileURL(specifier).toString();
   }
   return defaultResolve(specifier, context, defaultResolve);
-}
-
-/**
- *
- * @param {string} url
- * @return {Promise<string>}
- */
-export async function dynamicInstantiate(url) {
-  const urlParts = url.split("/node_modules/");
-  urlParts.pop();
-  const require = createRequire(`${urlParts.join("/node_modules/")}/noop.js`);
-  let dynModule = require(url.replace(/.*\/node_modules\//, ""));
-  if (dynModule.default && dynModule !== dynModule.default) {
-    dynModule = {
-      ...dynModule.default,
-      ...dynModule,
-    };
-  }
-
-  const linkKeys = Object.keys(dynModule);
-  const exports = dynModule.default ? linkKeys : [...linkKeys, "default"];
-
-  return {
-    exports,
-    execute: (module) => {
-      module.default.set(dynModule);
-      for (const linkKey of linkKeys) {
-        module[linkKey].set(dynModule[linkKey]);
-      }
-    },
-  };
 }
