cf98080b2f604de2d9bfe7f57a3399da43881398
Junxiao Shi <git@mail1.yoursunny.com>
2020-06-06 04:35:23 -0400

    v0.0.20200606

--- mk/publish-release.sh
+++ mk/publish-release.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 set -e
-npm run clean
+set -o pipefail
+
+npm whoami
+
+npm run build clean
+npm run lint-ci
+npm run literate lint
 npm run build
-npm run build strip
 npm test
 
 VERSION=0.0.$(date +%Y%m%d)$1
 pnpm recursive exec --filter ./packages -- bash -c 'node ../../mk/edit-packagejson.js V '$VERSION
 git commit -a -m 'v'$VERSION
 
-pnpm recursive exec --filter ./packages -- bash -c 'node ../../mk/edit-packagejson.js CDR '$VERSION' && npm publish --access public'
+pnpm recursive exec --filter ./packages -- bash -c 'node ../../mk/edit-packagejson.js CDR '$VERSION
+pnpm recursive exec --filter ./packages -- bash -c 'npm publish --access public'
 git checkout -- .
