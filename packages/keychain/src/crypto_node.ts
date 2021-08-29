d9612e8571f12385331584b30989df95fa391470
Junxiao Shi <git@mail1.yoursunny.com>
2021-08-29 22:33:08 +0000

    keychain: put peculiar-webcrypto.cjs in tarball

--- packages/keychain/src/crypto_node.ts
+++ packages/keychain/src/crypto_node.ts
@@ -1,6 +1,6 @@
 import * as nodeCrypto from "node:crypto";
 
-// @ts-expect-error typing error in @peculiar/webcrypto as of @types/web 0.0.26
+// @ts-expect-error typing error in @peculiar/webcrypto as of @types/web 0.0.27
 import { Crypto as peculiarCrypto } from "../peculiar-webcrypto.cjs";
 
 export const crypto: Crypto = (nodeCrypto.webcrypto as any) ?? new peculiarCrypto();
--- packages/keychain/src/mod.ts
+++ packages/keychain/src/mod.ts
@@ -1,6 +1,6 @@
-export { crypto as KeyChainImplWebCrypto } from "./crypto_node";
-export * as CertNaming from "./naming";
 export * from "./algo/mod";
-export * from "./key/mod";
 export * from "./cert/mod";
+export { crypto as KeyChainImplWebCrypto } from "./crypto_node";
+export * from "./key/mod";
+export * as CertNaming from "./naming";
 export * from "./store/mod";
