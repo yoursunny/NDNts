ce0ecfe70bee2e23ec89ef40416b42c77dceef84
Junxiao Shi <git@mail1.yoursunny.com>
2021-09-19 17:40:23 +0000

    mk: @types/* used in .d.ts should be dependencies

--- mk/list-outdated.mjs
+++ mk/list-outdated.mjs
@@ -1,11 +1,13 @@
-// This script lists dependencies where pnpm installed version differs from package.json specifier.
-const fs = require("graceful-fs");
-const yaml = require("js-yaml");
+#!/usr/bin/env node
+// List dependencies where pnpm installed version differs from package.json specifier.
+
+import fs from "graceful-fs";
+import yaml from "js-yaml";
 
 const doc = yaml.load(fs.readFileSync("pnpm-lock.yaml"));
-for (const [folder, { specifiers, dependencies, devDependencies }] of Object.entries(doc.importers)) {
+for (const [folder, { specifiers, dependencies = {}, devDependencies = {} }] of Object.entries(doc.importers)) {
   for (const list of [dependencies, devDependencies]) {
-    for (const [dep, version] of Object.entries(list || {})) {
+    for (const [dep, version] of Object.entries(list)) {
       const specifier = specifiers[dep];
       if (/[*:]/.test(specifier)) {
         continue;
