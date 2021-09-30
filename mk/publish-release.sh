69cb81ac451096dce860e8a729f86454bd2f3019
Junxiao Shi <git@mail1.yoursunny.com>
2021-09-30 01:39:51 +0000

    v0.0.20210930

--- mk/publish-release.sh
+++ mk/publish-release.sh
@@ -8,17 +8,17 @@ VERSION=0.0.$(git show -s --format='%ct' | gawk '{ print strftime("%Y%m%d", $1, 
 echo 'Publishing version '$VERSION >/dev/stderr
 
 npm whoami # check NPM login
-[[ $(git status --porcelain | tee >(cat 1>&2) | wc -l) -eq 0 ]] # code must be committed
+git diff --exit-code # code must be committed
 
 if [[ $NDNTS_SKIP_BUILD -ne 1 ]]; then
   npm run build clean
   npm run lint
-  [[ $(git status --porcelain | tee >(cat 1>&2) | wc -l) -eq 0 ]]
+  git diff --exit-code
   npm run build
   npm test
 fi
 
-RECURSE='./node_modules/.bin/pnpm --filter ./packages recursive exec'
+RECURSE='./node_modules/.bin/pnpm -r --filter ./packages exec'
 $RECURSE -- bash -c 'node '$ROOTDIR'/mk/edit-packagejson.js V '$VERSION
 git commit -a -m 'v'$VERSION
 
