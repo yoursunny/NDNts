ddfdf3b3a897d28a0a89601fe7549c137477a0fa
Junxiao Shi <git@mail1.yoursunny.com>
2021-10-27 23:00:20 +0000

    mk: make loader.mjs work with Node 16.12

--- mk/loader.mjs
+++ mk/loader.mjs
@@ -1,4 +1,5 @@
 import * as K from "@k-foss/ts-esnode";
+import fs from "graceful-fs";
 import path from "node:path";
 
 export const resolve = K.resolve;
@@ -10,4 +11,20 @@ export async function getFormat(url, context, defaultGetFormat) {
     return { format: "module" };
   }
   return defaultGetFormat(url, context, defaultGetFormat);
+}
+
+export async function load(url, context, defaultLoad) {
+  const { pathname } = new URL(url);
+  if (!pathname.endsWith(".ts")) {
+    return defaultLoad(url, context, defaultLoad);
+  }
+  const typescript = await fs.promises.readFile(pathname, { encoding: "utf-8" });
+  const { source } = await K.transformSource(typescript, {
+    format: "module",
+    url,
+  }, () => { throw new Error("unexpected call to defaultTransformSource"); });
+  return {
+    format: "module",
+    source,
+  };
 }
