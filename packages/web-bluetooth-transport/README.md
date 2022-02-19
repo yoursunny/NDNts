cf860648f24444b6080a6392429e475e53f12675
Junxiao Shi <git@mail1.yoursunny.com>
2022-02-19 16:17:57 +0000

    web-bluetooth-transport: delete rxPolling hack
    ESP32 NimBLE stack can work without using rxPolling.

--- packages/web-bluetooth-transport/README.md
+++ packages/web-bluetooth-transport/README.md
@@ -3,31 +3,34 @@
 This package is part of [NDNts](https://yoursunny.com/p/NDNts/), Named Data Networking libraries for the modern web.
 
 This package implements a Bluetooth Low Energy (BLE) client transport using [Web Bluetooth API](https://web.dev/bluetooth/).
-As of 2021-08-29, Web Bluetooth API is only available in Chrome-based browsers.
+As of 2022-02-19, Web Bluetooth API is only available in Chrome-based browsers.
 The protocol is compatible with [esp8266ndn](https://github.com/yoursunny/esp8266ndn) `BleServerTransport` class, summarized in the next section.
 
 Tested combinations:
 
-* Chrome 92 on Windows 10, nRF52832: works.
-* Chrome 92 on Windows 10, ESP32: works; however, the client is unable to enable notification (`startNotifications` throws error) and relies on `readValue` polling to receive packets, causing higher latency and increased power consumption.
-* Chrome on Android: does not work, [insufficient MTU](https://github.com/WebBluetoothCG/web-bluetooth/issues/284#issuecomment-244738626).
+* Chrome 98 on Windows 10, nRF52832: works.
+* Chrome 98 on Windows 10, ESP32 with NimBLE stack: works.
+* Chrome 98 on Windows 10, ESP32 with Bluedroid stack: does not work, "GATT Error: Not supported".
+* Chrome 98 on Android 11, nRF52832: works.
+* Chrome 98 on Android 11, ESP32 with NimBLE stack: works.
+* Chrome 98 on Android 11, ESP32 with Bluedroid stack: does not work, "GATT Error: Not supported".
 
 ## NDN-BLE protocol
 
 This protocol defines how to exchange NDN packets over Bluetooth Low Energy (BLE), between a BLE server (aka peripheral) and a BLE client (aka central).
 
 The server and client should negotiate the maximum possible MTU.
-This protocol does not define a fragmentation scheme.
+This protocol does not define a fragmentation scheme, although it can be used with existing NDN fragmentation schemes such as NDNLPv2.
 
 The server must provide a primary service, **NDN-BLE service**.
-Its UUID is 099577e3-0788-412a-8824-395084d97391.
+Its UUID is `099577e3-0788-412a-8824-395084d97391`.
 This service shall contain two characteristics: CS and SC.
 
-**CS characteristic** enables client-to-server transmission.
-Its UUID is cc5abb89-a541-46d8-a351-2f95a6a81f49.
+**CS characteristic** is for client-to-server transmission.
+Its UUID is `cc5abb89-a541-46d8-a351-2f95a6a81f49`.
 To transmit a packet from client to server, the client shall write a value to this characteristic.
 
-**SC characteristic** enables server-to-client transmission.
-Its UUID is 972f9527-0d83-4261-b95d-b1b2fc73bde4.
+**SC characteristic** is for server-to-client transmission.
+Its UUID is `972f9527-0d83-4261-b95d-b1b2fc73bde4`.
 The client should enable notifications on this characteristic.
 To transmit a packet from server to client, the server shall write a value to this characteristic, then post a notification.
--- packages/web-bluetooth-transport/src/web-bluetooth-transport.ts
+++ packages/web-bluetooth-transport/src/web-bluetooth-transport.ts
@@ -2,82 +2,40 @@ import "web-bluetooth";
 
 import { L3Face, rxFromPacketIterable, Transport } from "@ndn/l3face";
 import EventIterator from "event-iterator";
-import throat from "throat";
 
 const UUID_SVC = "099577e3-0788-412a-8824-395084d97391";
 const UUID_CS = "cc5abb89-a541-46d8-a351-2f95a6a81f49";
 const UUID_SC = "972f9527-0d83-4261-b95d-b1b2fc73bde4";
 
-function equalDataView(a: DataView, b: DataView): boolean {
-  if (a.byteLength !== b.byteLength) {
-    return false;
-  }
-  const lastWord = a.byteLength - a.byteLength % 8;
-  for (let i = 0; i < lastWord; i += 8) {
-    if (a.getBigUint64(i) !== b.getBigUint64(i)) {
-      return false;
-    }
-  }
-  for (let i = lastWord; i < a.byteLength; ++i) {
-    if (a.getUint8(i) !== b.getUint8(i)) {
-      return false;
-    }
-  }
-  return true;
-}
-
 /** Web Bluetooth transport. */
 export class WebBluetoothTransport extends Transport {
   public override readonly rx: Transport.Rx;
-  private mutex: (f: () => Promise<void>) => Promise<void>;
 
   private constructor(
       private readonly server: BluetoothRemoteGATTServer,
       private readonly cs: BluetoothRemoteGATTCharacteristic,
       sc: BluetoothRemoteGATTCharacteristic,
-      rxPolling: boolean,
   ) {
     super({
       describe: `WebBluetoothTransport(${server.device.id})`,
     });
 
-    let rxLast = new DataView(new ArrayBuffer(0));
-    if (rxPolling) {
-      this.mutex = throat(1);
-      void (async () => {
-        while (rxPolling) { // eslint-disable-line no-unmodified-loop-condition
-          await new Promise((r) => setTimeout(r, 50));
-          try {
-            await sc.readValue();
-          } catch {}
-        }
-      })();
-    } else {
-      this.mutex = (f) => f();
-    }
-
     this.rx = rxFromPacketIterable(new EventIterator<Uint8Array>(
       ({ push, stop }) => {
         const pushHandler = (evt: Event) => {
           const value = (evt.target as BluetoothRemoteGATTCharacteristic).value;
-          if (!value || (rxPolling && equalDataView(rxLast, value))) {
+          if (!value) {
             return;
           }
-          rxLast = value;
           push(new Uint8Array(value.buffer, value.byteOffset, value.byteLength));
         };
 
-        const stopHandler = () => {
-          rxPolling = false;
-          stop();
-        };
-
         sc.addEventListener("characteristicvaluechanged", pushHandler);
-        server.device.addEventListener("gattserverdisconnected", stopHandler);
+        server.device.addEventListener("gattserverdisconnected", stop);
 
         return () => {
           sc.removeEventListener("characteristicvaluechanged", pushHandler);
-          server.device.removeEventListener("gattserverdisconnected", stopHandler);
+          server.device.removeEventListener("gattserverdisconnected", stop);
         };
       },
     ));
@@ -90,7 +48,7 @@ export class WebBluetoothTransport extends Transport {
   public override readonly tx = async (iterable: AsyncIterable<Uint8Array>): Promise<void> => {
     try {
       for await (const pkt of iterable) {
-        await this.mutex(() => this.cs.writeValue(pkt));
+        await this.cs.writeValue(pkt);
       }
     } finally {
       this.close();
@@ -111,14 +69,8 @@ export class WebBluetoothTransport extends Transport {
     const svc = await server.getPrimaryService(UUID_SVC);
     const cs = await svc.getCharacteristic(UUID_CS);
     const sc = await svc.getCharacteristic(UUID_SC);
-
-    let rxPolling = false;
-    try {
-      await sc.startNotifications();
-    } catch {
-      rxPolling = true;
-    }
-    return new WebBluetoothTransport(server, cs, sc, rxPolling);
+    await sc.startNotifications();
+    return new WebBluetoothTransport(server, cs, sc);
   }
 }
 
