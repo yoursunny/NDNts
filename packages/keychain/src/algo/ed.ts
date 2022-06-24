da5e3577f8c3733a286b5f61c11278591fe003fc
Junxiao Shi <git@mail1.yoursunny.com>
2022-06-24 01:27:26 +0000

    keychain: change Ed25519 algorithm identifier for Node 18.4.0

--- packages/keychain/src/algo/ed.ts
+++ packages/keychain/src/algo/ed.ts
@@ -1,29 +1,18 @@
 import { LLSign, LLVerify, SigType, Verifier } from "@ndn/packet";
 import { crypto } from "@ndn/util";
 import * as asn1 from "@yoursunny/asn1";
-import { polyfillEd25519 } from "@yoursunny/webcrypto-ed25519";
+import { Ed25519Algorithm, polyfillEd25519 } from "@yoursunny/webcrypto-ed25519";
 
 import type { CryptoAlgorithm, SigningAlgorithm } from "../key/mod";
 import { extractSpkiAlgorithm } from "./impl-spki";
 
 polyfillEd25519();
 
-interface NodeEdKeyGenParams {
-  name: string;
-  namedCurve: string;
-}
-
-interface NodeEdKeyImportParams {
-  name: string;
-  namedCurve: string;
-  public?: boolean;
-}
-
 class NodeEd implements SigningAlgorithm<{}, true, {}> {
   constructor(
       public readonly uuid: string,
       public readonly sigType: number,
-      private readonly params: NodeEdKeyGenParams & NodeEdKeyImportParams,
+      private readonly algo: KeyAlgorithm,
       private readonly oid: string,
   ) {}
 
@@ -38,19 +27,19 @@ class NodeEd implements SigningAlgorithm<{}, true, {}> {
     if (importPkcs8) {
       const [pkcs8, spki] = importPkcs8;
       [privateKey, publicKey] = await Promise.all([
-        crypto.subtle.importKey("pkcs8", pkcs8, this.params, extractable, this.keyUsages.private),
-        crypto.subtle.importKey("spki", spki, this.params, true, this.keyUsages.public),
+        crypto.subtle.importKey("pkcs8", pkcs8, this.algo, extractable, this.keyUsages.private),
+        crypto.subtle.importKey("spki", spki, this.algo, true, this.keyUsages.public),
       ]);
     } else {
-      ({ privateKey, publicKey } = await crypto.subtle.generateKey(this.params, extractable,
-        [...this.keyUsages.private, ...this.keyUsages.public]));
+      ({ privateKey, publicKey } = await crypto.subtle.generateKey(this.algo, extractable,
+        [...this.keyUsages.private, ...this.keyUsages.public]) as CryptoKeyPair);
     }
 
     const spki = new Uint8Array(await crypto.subtle.exportKey("spki", publicKey));
     return {
       privateKey,
       publicKey,
-      jwkImportParams: this.params,
+      jwkImportParams: this.algo,
       spki,
       info: {},
     };
@@ -58,10 +47,10 @@ class NodeEd implements SigningAlgorithm<{}, true, {}> {
 
   public async importSpki(spki: Uint8Array, der: asn1.ElementBuffer) {
     if (extractSpkiAlgorithm(der) !== this.oid) {
-      throw new Error(`not ${this.params.name} key`);
+      throw new Error(`not ${this.algo.name} key`);
     }
     const key = await crypto.subtle.importKey(
-      "spki", spki, this.params, true, this.keyUsages.public);
+      "spki", spki, this.algo, true, this.keyUsages.public);
     return {
       publicKey: key,
       spki,
@@ -71,14 +60,14 @@ class NodeEd implements SigningAlgorithm<{}, true, {}> {
 
   public makeLLSign({ privateKey }: CryptoAlgorithm.PrivateKey<{}>): LLSign {
     return async (input) => {
-      const raw = await crypto.subtle.sign(this.params.name, privateKey, input);
+      const raw = await crypto.subtle.sign(this.algo, privateKey, input);
       return new Uint8Array(raw);
     };
   }
 
   public makeLLVerify({ publicKey }: CryptoAlgorithm.PublicKey<{}>): LLVerify {
     return async (input, sig) => {
-      const ok = await crypto.subtle.verify(this.params.name, publicKey, sig, input);
+      const ok = await crypto.subtle.verify(this.algo, publicKey, sig, input);
       Verifier.throwOnBadSig(ok);
     };
   }
@@ -94,10 +83,7 @@ interface EdGenParams {
 export const Ed25519: SigningAlgorithm<{}, true, {}> = new NodeEd(
   "fa9e8104-39b1-4a8e-828d-8c557d973476",
   SigType.Ed25519,
-  {
-    name: "NODE-ED25519",
-    namedCurve: "NODE-ED25519",
-  },
+  Ed25519Algorithm,
   "2B6570", // 1.3.101.112
 );
 
