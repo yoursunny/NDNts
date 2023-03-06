294b951c22936332b129a7e1d52132ce4e616f6d
Junxiao Shi <git@mail1.yoursunny.com>
2023-03-06 04:18:15 +0000

    dpdkmgmt: GraphQL typing

--- packages/dpdkmgmt/src/prefix-reg.ts
+++ packages/dpdkmgmt/src/prefix-reg.ts
@@ -1,9 +1,28 @@
 import { ReadvertiseDestination } from "@ndn/fw";
 import type { Name } from "@ndn/packet";
-import { type GraphQLClient, gql } from "graphql-request";
+import { type GraphQLClient, type Variables, gql } from "graphql-request";
 
 interface State {
   fibEntryID?: string;
+}
+
+interface InsertFibEntryVars extends Variables {
+  name: string;
+  nexthops: string[];
+}
+
+interface InsertFibEntryResp {
+  insertFibEntry: {
+    id: string;
+  };
+}
+
+interface DeleteVars extends Variables {
+  id: string;
+}
+
+interface DeleteResp {
+  delete: boolean;
 }
 
 /** Enable prefix registration via NDN-DPDK GraphQL management API. */
@@ -13,7 +32,7 @@ export class NdndpdkPrefixReg extends ReadvertiseDestination<State> {
   }
 
   protected override async doAdvertise(name: Name, state: State) {
-    const { insertFibEntry: { id } } = await this.client.request(gql`
+    const resp = await this.client.request<InsertFibEntryResp, InsertFibEntryVars>(gql`
       mutation insertFibEntry($name: Name!, $nexthops: [ID!]!, $strategy: ID) {
         insertFibEntry(name: $name, nexthops: $nexthops, strategy: $strategy) {
           id
@@ -23,7 +42,7 @@ export class NdndpdkPrefixReg extends ReadvertiseDestination<State> {
       name: name.toString(),
       nexthops: [this.faceID],
     });
-    state.fibEntryID = id;
+    state.fibEntryID = resp.insertFibEntry.id;
   }
 
   protected override async doWithdraw(name: Name, state: State) {
@@ -31,7 +50,7 @@ export class NdndpdkPrefixReg extends ReadvertiseDestination<State> {
     if (!state.fibEntryID) {
       return;
     }
-    await this.client.request(gql`
+    await this.client.request<DeleteResp, DeleteVars>(gql`
       mutation delete($id: ID!) {
         delete(id: $id)
       }
