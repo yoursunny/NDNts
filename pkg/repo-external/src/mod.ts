271da0e5b207c7aeeebd5226a00c5cdc418c7e20
Junxiao Shi <git@mail1.yoursunny.com>
2024-07-01 03:27:40 +0000

    repo-external+sync: delete

--- pkg/repo-external/src/mod.ts
+++
@@ -1,6 +0,0 @@
-export {
-  /** @deprecated Import from `@ndn/pyrepo`. */ PrpsPublisher,
-  /** @deprecated Import from `@ndn/pyrepo`. */ PrpsSubscriber,
-  /** @deprecated Import from `@ndn/pyrepo`. */ PyRepoClient,
-  /** @deprecated Import from `@ndn/pyrepo`. */ PyRepoStore,
-} from "@ndn/pyrepo";
