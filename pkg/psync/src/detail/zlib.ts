ffa57d00794d3d4ba997bf4a701a911a4dce51d0
Junxiao Shi <git@mail1.yoursunny.com>
2024-09-20 17:31:32 +0000

    mk: upgrade to tslib 2.7.0

--- pkg/psync/src/detail/zlib.ts
+++ pkg/psync/src/detail/zlib.ts
@@ -1,14 +1,14 @@
-import pako from "pako";
+import { deflate, inflate } from "pako";
 
 import type { Compression } from "./iblt-codec";
 
 export function makeZlib(level: pako.DeflateFunctionOptions["level"]): Compression {
   return {
     compress(input) {
-      return pako.deflate(input, { level });
+      return deflate(input, { level });
     },
     decompress(compressed) {
-      return pako.inflate(compressed);
+      return inflate(compressed);
     },
   };
 }
